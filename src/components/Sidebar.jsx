03db6d8ae459d3582c5fa9fa83536659de5f57e8
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-02 18:07:20 -0300

    feat: adicionando Modal na Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 
-
-import SocialNetworks from './SocialNetworks';
-import InformationContainer from './InformationContainer';
+import SocialNetworks from './SocialNetworks.jsx';
+import InformationContainer from './InformationContainer.jsx';
+import Modal from './Modal.jsx'
 
 import Avatar from '../img/perfil.jpg';
 
@@ -9,18 +10,24 @@ import '../styles/components/sidebar.sass';
 
 
 const Sidebar = () => {
+    const [ isModalOpen, setIsModalOpen ] = useState(false);
 
-    function windowAlert(){
-        window.alert('infelizmente')
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
     }
 
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    }
+    
     return(
         <aside id="sidebar">
             <img src={Avatar} alt="perfil" />
             <p className="title">Full Stack Web Development</p>
             <SocialNetworks />
             <InformationContainer />
-            <a href="" className="btn" onClick={windowAlert}>Download curriculo</a>
+            <a className="btn" onClick={handleOpenModal}>Download curriculo</a>
+            <Modal isOpen={isModalOpen} onClose={handleCloseModal}/>
         </aside>
     )
 }
